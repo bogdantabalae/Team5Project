c79ee94ff430eb522decacaa759a29d4469b06e2
bogdantabalae <bogdantabalae@yahoo.com>
2026-04-21 00:18:27 +0100

    Made the home page be the default

--- main project/frontend/src/app/app.routes.ts
+++ main project/frontend/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { StoreComponent } from './store/store';
 import { AdminComponent } from './admin/admin';
+import { HomeComponent } from './home/home';
 import { adminGuard } from './admin.guard';
 
 export const routes: Routes = [
-  { path: '',      redirectTo: 'login', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'store', component: StoreComponent },
   { path: 'admin', component: AdminComponent, canActivate: [adminGuard] },
